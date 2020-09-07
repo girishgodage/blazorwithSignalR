440eb80173807182dd4a8dc660273998db7541e9
girish <girishgodage@gmail.com>
2020-09-07 17:51:06 +0530

    first commit

---
+++ BlazorRealTime/BlazorRealTime.Server/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorRealTime.Server
+{
+    public class NotificationHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+    }
+}
